d6db5c43eba4cd6f7287b51015d43fec0b3a5e19
Ryan Breeden <ryan@ryanbreeden.com>
2017-05-04 09:00:41 -0700

    Homework progression 1 - JSON complete - getElementsByClassName in progress

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,45 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+//document.body - returns the <body> or <frameset> node of the current document, or null if no such element exists. 
+//element.childNodes - returns a collection of a node's child nodes, as a NodeList object
+//element.classList - returns the class name(s) of an element, as a DOMTokenList object
+var getElementsByClassName = function(className) {
   // your code here
+  var children = document.body.childNodes;
+  var classArr = [];
+ 
+  //console.log("children: " + children);
+  //console.log("documentchild: " + document.body.childElementCount)
+
+  //check body for className
+  if(document.body.classList === className){
+  	classArr.push(document.body);
+  }
+  //check children for className
+  for (var i = 0; i < document.body.childElementCount; i++){
+    //check if each node has the class
+    for(var x = 0; x < children.classList.length; x++){
+      if (children.classList[x] === className){
+      	classArr.push(children[i]);
+      }
+    }  
+  }
+  
+  function recursion(node, className){
+    //base case 
+    if(node.childNodes.length === 0){
+      if(node.classList === className){
+      	return [node];
+      //if does not match
+      } else {
+      	return [];
+      }
+    }
+    //if node has children 
+    for (var i = 0; i < node.length; i++){
+      var result = recursion(node.childNodes[i]);
+    }
+  }
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,82 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+console.log(obj);
+  //STRING
+  if (typeof obj === "string"){
+  	var str = '"';
+  	str += obj;
+  	str += '"';
+  	return str;
+  }
+  //NUMBER
+  if (typeof obj === "number"){
+  	return obj.toString();
+  }
+  //BOOLEAN
+  if (typeof obj === "boolean"){
+  	return obj.toString();
+  }
+  //NULL
+  if (obj === null){
+    return "null";
+  }
+  //NaN 
+  if (obj === NaN){
+  	return "NaN";
+  }
+  //UNDEFINED
+  if (obj === undefined){
+  	return "undefined";
+  }
+
+  //ARRAY
+  if (Array.isArray(obj)){
+    var str = '[';
+    for (var i = 0; i < obj.length; i++){
+      str += stringifyJSON(obj[i]);
+      //add commas if not on the last index
+      if (i !== obj.length - 1){
+    		str += ',';
+      }
+    }
+    str += ']';
+    return str;
+  }
+
+  //OBJECT
+  if (typeof obj === "object"){
+  	var str = '{';
+  	var count = 0;
+  	var countWritten = 0;
+  	//count the properties in obj
+  	for (var key in obj){
+  		count++;
+  	}
+  	//write the string from the keys, exclude the last property
+  	for (var key in obj){
+  	  if(typeof obj[key] === "undefined") continue; 
+  	  if(typeof obj[key] === "function") continue;
+  	  if(countWritten < count - 1){
+  	  	str += stringifyJSON(key); 
+  	  	str += ':';
+  	  	str += stringifyJSON(obj[key]);
+  	  	str += ",";
+  	  	countWritten++;
+  	  //write the last line without the comma at the end
+  	  } else if (countWritten === count - 1){ 
+  	    str += stringifyJSON(key); 
+  	    str += ':';
+  	    str += stringifyJSON(obj[key]); 
+  	    countWritten++;
+  	  }
+  	}//end for
+  	str += '}'
+  	return str;
+  }
 };
+
+
+
+
+
